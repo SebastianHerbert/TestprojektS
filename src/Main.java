c5bf242585c8b8652c7c9d475d5029b375e99154
SebastianHerbert <herbert.sebastian@outlook.com>
2016-11-16 11:25:57 +0100

    First

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,55 @@
+import java.awt.Color;
+
+import javax.swing.*;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// test
+		JFrame frame = new JFrame ("titel");
+		SpringLayout layout = new SpringLayout();
+		
 
+
+		frame.setSize(500, 250);
+		//frame.setLayout(layout);
+		//frame.setVisible(true);
+		//frame.add(new JLabel("Label: "));
+		//frame.add(new JTextField("Text field", 15));
+	    // main-Methode
+	   
+	        // Erzeugung eines neuen Dialoges
+	        JDialog meinJDialog = new JDialog();
+	        meinJDialog.setTitle("JPanel Beispiel");
+	        meinJDialog.setSize(450,300);
+
+	        // Hier erzeugen wir unsere JPanels
+	        JPanel einnahme = new JPanel();
+	        JPanel ausgabe = new JPanel();
+	        JPanel sonstiges = new JPanel();
+	
+	        // Hier setzen wir die Hintergrundfarben für die JPanels
+	        einnahme.setBackground(Color.RED);
+	        ausgabe.setBackground(Color.BLUE);
+	        sonstiges.setBackground(Color.GREEN);
+	 
+	 
+	        // Erzeugung eines JTabbedPane-Objektes
+	        JTabbedPane tabpane = new JTabbedPane
+	            (JTabbedPane.TOP,JTabbedPane.SCROLL_TAB_LAYOUT );
+	 
+	        // Hier werden die JPanels als Registerkarten hinzugefügt
+	        tabpane.addTab("Einnahmen", einnahme);
+	        tabpane.addTab("Ausgaben", ausgabe);
+	        tabpane.addTab("Sonstiges", sonstiges);
+	 
+	        // JTabbedPane wird unserem Dialog hinzugefügt
+	        meinJDialog.add(tabpane);
+	        // Wir lassen unseren Dialog anzeigen
+	        meinJDialog.setVisible(true);
+		
 	}
 
 }
